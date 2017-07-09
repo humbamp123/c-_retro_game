bad6b5faeb9da8e546fc1aca0102300f91c3540d
Andres Pineda <humb.amp123@gmail.com>
2017-07-08 19:43:18 -0700

    clean

--- src/classes/Game.class.cpp
+++ src/classes/Game.class.cpp
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Enemy.class.hpp"
+#include "Player.class.hpp"
 #include "Game.class.hpp"
 
 Game::Game() {
@@ -21,7 +23,7 @@ Game::Game() {
   refresh();//must be used after any changes have been made
   keypad(wnd, true); // Allows keys to be interpreted for actions
   nodelay(wnd, true); // This diables stoping everything when using wgetch()
-  curs_set(1); // Makes the cursor visible or invisible
+  curs_set(0); // Makes the cursor visible or invisible
   if(!has_colors()) //Macro to check if the terminal supports color
   {
       endwin();
@@ -46,14 +48,46 @@ Game::~Game(){
 
 
 void Game::run() {
-  move(5, 5); //moves the cursor to the specified y, x, position
-  std::string text = "Hello world!";
-  for(size_t i = 0; i < text.size(); i++) {
-      addch(text[i]); // prints a character at the current cursor position and then advances one space
-      addch(' ');
-  }
-
+  Player master;
   refresh(); //must be used after any changes have been made
 
-  while(1);
+  while(1) {
+    Enemy arbiter;
+  }
 }
+
+  // move(5, 5); //moves the cursor to the specified y, x, position
+  // std::string text = "Hello world!";
+  // for(size_t i = 0; i < text.size(); i++) {
+  //     addch(text[i]); // prints a character at the current cursor position and then advances one space
+  //     addch(' ');
+  // }
+
+  // Player master;
+  // Player chief(master);
+  // Player masterchief;
+  // masterchief = chief;
+
+  // std::cout << masterchief.getX() << std::endl;
+  // std::cout << masterchief.getY() << std::endl;
+  // std::cout << masterchief.getHitPoints() << std::endl;
+  // std::cout << masterchief.getMaxHitPoints() << std::endl;
+  // std::cout << masterchief.getEnergyPoints() << std::endl;
+  // std::cout << masterchief.getMaxEnergyPoints() << std::endl;
+  // std::cout << masterchief.getAttackDamage() << std::endl;
+  // std::cout << masterchief.getSprite() << std::endl;
+
+  // std::cout << "--------------------------" << std::endl;
+
+  // Enemy arbiter;
+  // Enemy elite(arbiter);
+  // Enemy halo;
+  // halo = elite;
+
+  // std::cout << halo.getX() << std::endl;
+  // std::cout << halo.getY() << std::endl;
+  // std::cout << halo.getHitPoints() << std::endl;
+  // std::cout << halo.getMaxHitPoints() << std::endl;
+  // std::cout << halo.getEnergyPoints() << std::endl;
+  // std::cout << halo.getMaxEnergyPoints() << std::endl;
+  // std::cout << halo.getSprite() << std::endl;
--- src/main.cpp
+++ src/main.cpp
@@ -3,50 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
+/*   By: apineda <apineda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/08 17:25:13 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/07/08 19:28:37 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/07/08 19:33:30 by apineda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "classes/Enemy.class.hpp"
-#include "classes/Player.class.hpp"
 #include "classes/Game.class.hpp"
 
   WINDOW* wnd;
 
 int main(void) {
-  // Game newGame;
+  Game newGame;
+  newGame.run();
 
-  // newGame.run();
-  Player master;
-  Player chief(master);
-  Player masterchief;
-  masterchief = chief;
-
-  std::cout << masterchief.getX() << std::endl;
-  std::cout << masterchief.getY() << std::endl;
-  std::cout << masterchief.getHitPoints() << std::endl;
-  std::cout << masterchief.getMaxHitPoints() << std::endl;
-  std::cout << masterchief.getEnergyPoints() << std::endl;
-  std::cout << masterchief.getMaxEnergyPoints() << std::endl;
-  std::cout << masterchief.getAttackDamage() << std::endl;
-  std::cout << masterchief.getSprite() << std::endl;
-
-  std::cout << "--------------------------" << std::endl;
-
-  Enemy arbiter;
-  Enemy elite(arbiter);
-  Enemy halo;
-  halo = elite;
-
-  std::cout << halo.getX() << std::endl;
-  std::cout << halo.getY() << std::endl;
-  std::cout << halo.getHitPoints() << std::endl;
-  std::cout << halo.getMaxHitPoints() << std::endl;
-  std::cout << halo.getEnergyPoints() << std::endl;
-  std::cout << halo.getMaxEnergyPoints() << std::endl;
-  std::cout << halo.getSprite() << std::endl;
   return (0);
 }
